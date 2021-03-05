7ed64669da92149593dd58cf1bc97239bbd5b833
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-03-05 16:49:07 +0200

    add helpers

---
+++ src/main/java/helpers/ConsoleTweaks.java
@@ -0,0 +1,15 @@
+package helpers;
+
+public class ConsoleTweaks {
+    public void printDashSeparator() {
+        System.out.println("----------------------------");
+    }
+
+    public void printEmptyLine() {
+        System.out.println();
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
